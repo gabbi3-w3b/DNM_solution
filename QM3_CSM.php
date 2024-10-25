f9ff83d0a3567e8cda03d5655da665bee254f6b6
gabbi3-w3b <gabrielsabo200@gmail.com>
2024-10-25 16:04:35 +0100

    Update QM3_CSM.php

--- QM3_CSM.php
+++ QM3_CSM.php
@@ -98,8 +98,8 @@
                         </div>
 
                         <button type="submit" class="btn btn-secondary" name="find">Factorize</button>
-                    <a href="../Personal_Blog/QM2_FzM.html" class="btn btn-info">Return</a>
-                    <a href="../Personal_Blog/_____QMain____.html" class="btn btn-danger">Home page</a>
+                    <a href="QM2_FzM.html" class="btn btn-info">Return</a>
+                    <a href="index.html" class="btn btn-danger">Home page</a>
                     </form>
 
                 </div>
